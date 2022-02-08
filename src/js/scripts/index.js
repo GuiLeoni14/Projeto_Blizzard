cac86867c8104e3859e4172010203cc836000729
GuiLeoni14 <guileone167@gmail.com>
2022-02-08 12:23:52 -0200

    ajustando hero

--- src/js/scripts/index.js
+++ src/js/scripts/index.js
@@ -2,6 +2,20 @@ AOS.init({
     once: true,
     disable: 'mobile',
 });
+var slide_thumbnail = new Swiper(".slide-thumbnail", {
+    slidesPerView: 5,
+    direction: 'vertical',
+    spaceBetween: 20,
+    watchSlidesProgress: true
+  });
+
 var slide_hero = new Swiper(".slide-principal", {
     effect: 'fade',
+    thumbs: {
+        swiper:slide_thumbnail,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false, // quando passar passar os slide manualmente para não desativar o autoplay(por padrão TRUE ele desativa)
+    }
   });
